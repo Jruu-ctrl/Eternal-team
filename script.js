7bd0000093be8e0f44ec78b70fc5440d40144e8a
Adam <jruanticheat@gmail.com>
2025-01-17 20:29:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://bf9e896f-5f7a-4ada-a72b-a9ff2585bb28-00-k28lga9q4vqa.picard.replit.dev/submit_form", {
+fetch("http://172.31.196.71:8080/submit_form", {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
