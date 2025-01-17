1de79a25b104cf01beaf55bde6f72832252c3d93
Adam <jruanticheat@gmail.com>
2025-01-17 20:24:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-document.getElementById('reviewForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const review = document.getElementById('review').value;
-    fetch("http://172.31.196.71:8080", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            user_name: name,
-            message: review
-        })
+fetch("https://bf9e896f-5f7a-4ada-a72b-a9ff2585bb28-00-k28lga9q4vqa.picard.replit.dev/submit_form", {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        user_name: name,
+        message: review
     })
-    .then(response => response.json())
-    .then(data => {
-        alert('Review submitted successfully!');
-        document.getElementById('reviewForm').reset();
-    })
-    .catch(error => {
-        alert('Error submitting review.');
-    });
+})
+.then(response => response.json())
+.then(data => {
+    alert('Review submitted successfully!');
+    document.getElementById('reviewForm').reset();
+})
+.catch(error => {
+    alert('Error submitting review.');
 });
