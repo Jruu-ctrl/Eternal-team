ce0bec9a6a113b98dfaea77675d473f9ca22f7b8
Jruu <jruanticheat@gmail.com>
2025-02-23 00:27:19 +0100

    add section lottery

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
-fetch("http://172.31.196.71:8080/submit_form", {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-        user_name: name,
-        message: review
-    })
-})
-.then(response => response.json())
-.then(data => {
-    alert('Review submitted successfully!');
-    document.getElementById('reviewForm').reset();
-})
-.catch(error => {
-    alert('Error submitting review.');
-});
+let timeoutid
+function coord() {
+    var name = prompt("username->")
+    document.getElementById("name").innerHTML = name
+}
+function verif(x) {
+    var choix = document.getElementById(x).value
+    if (choix > 20 || 5 > choix) {
+        alert("Your number must be between 5 and 20. You entered: " + choix + " (Invalid).")
+        document.getElementById(x).value = ""
+    }
+}
+function test() {
+    tab = document.getElementsByName('inp')
+    for (let i = 0; i < 6; i++) {
+        if (tab[i].value > 5 && tab[i].value < 20) { return true }
+        else { return false }
+    }
+}
+function lottery() {
+    if (test()) {
+        out = document.getElementsByName('out')
+        document.getElementById("stop").disabled = false
+        document.getElementById("start").disabled = true
+        let i = 0
+        while (i < 6) {
+            out[i].value = Math.trunc(Math.random() * (21 - 5) + 5)
+            i++
+        }
+        timeoutid = setTimeout(lottery, 50)
+        return out
+    }
+}
+
+function calc() {
+    stop()
+    res = document.getElementById("res")
+    tab = document.getElementsByName('inp')
+    ans = document.getElementsByName('out')
+    nb = 0
+    for (let i = 0; i < 6; i++) {
+        if (tab[i].value == ans[i].value) { nb++ }
+    }
+    if (nb == 0) { res.innerHTML = "😥sorry😥" }
+    else { res.innerHTML = "🤑congratulation🤑 with: " + nb + " answer" }
+
+}
+function stop() {
+    clearTimeout(timeoutid)
+    document.getElementById("stop").disabled = true
+    document.getElementById("start").disabled = true
+}
